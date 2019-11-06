f282f33ec30f689d8a895ffc93d4ee9cc964b229
josephine-pn3ed <josephine.morre@student.passerellesnumeriques.org>
2019-11-06 14:08:45 +0800

    connected

--- scripts/controller.js
+++ scripts/controller.js
@@ -4,32 +4,46 @@ $(document).ready(function() {
 		client = mqtt.connect("wss://test.mosquitto.org:8081/mqtt");
 		client.on("connect", function(){
 		    console.log("Successfully connected");
+		
+			client.subscribe("aspire/device");
+			client.subscribe("Hannah/Patrick/");
+			client.subscribe("elective_c");
+			client.subscribe("paho/temperature");
+			client.subscribe("z");
+			client.subscribe("lalaine/jake");
+			client.subscribe("sensor/tanilonnadela");
+			client.subscribe("bargs/jane");
+			client.subscribe("mogal");
+			client.subscribe("princechan");
+			client.subscribe("group1/temperature");
+			client.subscribe("genjess");
 		})
+
 		client.on("message", function (topic, payload) {
 			console.log("Received { topic:"+topic+"; payload: "+payload+" }");
 			switch (topic) {
 				case "aspire/device":
 					$('#yc').val(payload);
 					break;
-				case "hannah-patrick":
+				case "Hannah/Patrick/":
 					$('#hp').val(payload);
 					break;
 				case "elective_c":
 					$('#ir').val(payload);
 					break;
-				case "Rivastibs":
+				case "paho/temperature":
 					$('#tj').val(payload);
 					break;
-				case "marionivy":
+				case "z":
 					$('#im').val(payload);
 					break;
-				case "lalaineJake":
+				case "lalaine/jake":
 					$('#mj').val(payload);
 					break;
-				case "sensor/temperature/tanilon":
+				case "sensor/tanilonnadela":
 					$('#ck').val(payload);
 					break;
-				case "bargs-jane":
+				case "bargs/jane":
 					$('#rj').val(payload);
 					break;
 				case "mogal":
@@ -38,7 +52,7 @@ $(document).ready(function() {
 				case "princechan":
 					$('#cp').val(payload);
 					break;
-				case "group1-temperature":
+				case "group1/temperature":
 					$('#js').val(payload);
 					break;
 				case "genjess":
